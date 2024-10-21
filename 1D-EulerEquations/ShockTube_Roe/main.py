4f2711eca77cce9430db5a269ddd977702e1b780
Francesco Neri <francesconeri95@gmail.com>
2024-10-21 15:39:40 +0200

    update file location

--- 1D-EulerEquations/ShockTube_Roe/main.py
+++ 1D-EulerEquations/ShockTube_Roe/main.py
@@ -7,7 +7,7 @@ INPUT PARAMETERS FOR THE SHOCK-TUBE PROBLEM
 """
 LENGTH = 1
 NX = 100
-TIME_MAX = 0.25
+TIME_MAX = 2  # to simulate two reflections
 RHOL, RHOR = 1.0, 0.125
 UL, UR = 0.0, 0.0
 PL, PR = 1.0, 0.1
@@ -21,7 +21,7 @@ Solution Driver
 x = np.linspace(0, LENGTH, NX)
 dx = x[1]-x[0]
 approxSpeed = np.sqrt(1.4*np.max([PL, PR])/np.min([RHOL, RHOR]))  # brutal approximation max eigenvalue
-CFLmax = 0.5  # conservative CFL
+CFLmax = 1.0  # conservative CFL
 dtMax = CFLmax* dx / approxSpeed
 nt = int(TIME_MAX/dtMax)
 t = np.linspace(0, TIME_MAX, nt)
@@ -34,7 +34,7 @@ tube.InitialConditionsLeftRight(inCondDict)
 tube.SetBoundaryConditions('reflective', 0)
 
 tube.SolveSystem(flux_method='Roe')
-tube.SaveSolution(folder_name='solutions', file_name='SodsTest_transparent_tMax_%.2f' %TIME_MAX)
+tube.SaveSolution(folder_name='solutions', file_name='SodsTest_tMax_%.2f' %TIME_MAX)
 tube.ShowAnimation()
 
 
